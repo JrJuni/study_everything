13f9ee1c02ca6089c2413249b142fab0b6f859c2
JrJuni <behomely0409@gmail.com>
2025-06-01 15:49:33 +0900

    250528_studyplus

--- study_AI/torch_basic.py
+++ study_AI/torch_basic.py
@@ -1,9 +1,14 @@
 import torch
 
+'''
+# Autograd : Sample code
 x = torch.tensor([2.0], requires_grad=True)
 y = x**2 + 2*x + 1
 print(f"y: {y}")
 
 y.backward()
-
 print(f"dy/dx: {x.grad}")
+'''
+
+
+
